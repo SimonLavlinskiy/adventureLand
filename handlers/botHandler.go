0b3ca863a038830fc38e09439b42e0a7a0fd5949
jake4take <jenchek95@gmail.com>
2022-01-26 18:55:22 +0300

    add button

--- handlers/botHandler.go
+++ handlers/botHandler.go
@@ -1,7 +1,21 @@
 package handlers
 
 import (
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+var msg tgbotapi.MessageConfig
+
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Карта"),
+		tgbotapi.NewKeyboardButton("Инвентарь"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Жопа"),
+		tgbotapi.NewKeyboardButton("Попа"),
+	),
 )
 
 func GetMessage(telegramApiToken string) {
@@ -9,7 +23,7 @@ func GetMessage(telegramApiToken string) {
 	if err != nil {
 		panic(err)
 	}
-	bot.Debug = true
+	bot.Debug = false
 
 	updateConfig := tgbotapi.NewUpdate(0)
 
@@ -23,12 +37,22 @@ func GetMessage(telegramApiToken string) {
 			continue
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		newMessage := update.Message.Text
 
-		msg.ReplyToMessageID = update.Message.MessageID
+		if newMessage == "Карта" {
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "\U0001F7E9\U0001F7E7\U0001F7E9\U0001F7E9\U0001F7E9\U0001F7E9🌲🐺\n\U0001F7E9\U0001F7E7\U0001F7E9\U0001F7E9\U0001F7E9\U0001F7E9🌲\U0001F7E9\n\U0001F7E9\U0001F7E7\U0001F7E7\U0001F7E7\U0001F7E7\U0001F7E9\U0001F7E9\U0001F7E9\n\U0001F7E9\U0001F7E7🌳🌲\U0001F7E7\U0001F7E9\U0001F7E9\U0001F7E9\n\U0001F7E9\U0001F7E7🚪🌳🐱\U0001F7E9\U0001F7E7\U0001F7E7\n\U0001F7E9\U0001FAA8🌳🌲\U0001F7E7\U0001F7E9\U0001F7E7\U0001FAA8\n\U0001FAA8\U0001F7E9\U0001F7E7\U0001F7E7\U0001F7E7\U0001F7E7\U0001F7E7\U0001FAA8\n\U0001F7E9\U0001F7E9\U0001F7E7🌳🍎\U0001F7E9\U0001F7E9\U0001F7E9")
+			fmt.Println()
+		} else if newMessage == "/menu" {
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Сам ты "+update.Message.Text)
+			msg.ReplyMarkup = numericKeyboard
+		} else {
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Сам ты "+update.Message.Text)
+		}
 
 		if _, err := bot.Send(msg); err != nil {
 			panic(err)
 		}
+		//msg.ReplyToMessageID = update.Message.MessageID
+
 	}
 }
