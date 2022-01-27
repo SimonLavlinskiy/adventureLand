a9c7e4de530d9ca4a0a3a3ba74e7aebd4e9956c3
jake4take <jenchek95@gmail.com>
2022-01-27 15:44:25 +0300

    something

--- handlers/botHandler.go
+++ handlers/botHandler.go
@@ -9,32 +9,33 @@ var msg tgbotapi.MessageConfig
 
 // TODO вынести костантные названия кнопок в отдельный файл(Можно даже в yml)
 
-var mainKeyboardNames = [][]string{
-	{"Карта", "👜 Инвентарь 👜"},
-	{"/menu"},
+var mainKeyboardNames = []string{
+	"Карта", "👜 Инвентарь 👜",
+}
+
+var menuButtons = []string{
+	"/menu",
 }
 
 var backpackKeyboardNames = [][]string{
 	{"\U0001F9BA Шмот \U0001F9BA", "\"🍕 Еда 🍕\""},
-	{"/menu"},
 }
 
-func names2buttons(names [][]string) [][]tgbotapi.KeyboardButton {
-	var rows [][]tgbotapi.KeyboardButton
+func names2buttons(names []string) []tgbotapi.KeyboardButton {
+	var row []tgbotapi.KeyboardButton
 	for _, l := range names {
-		var row []tgbotapi.KeyboardButton
-		for _, s := range l {
-			row = append(row, tgbotapi.NewKeyboardButton(s))
-		}
-		rows = append(rows, tgbotapi.NewKeyboardButtonRow(row...))
+		row = append(row, tgbotapi.NewKeyboardButton(l))
 	}
-	return rows
+	return row
 }
 
 var mainKeyboard = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
 		tgbotapi.NewKeyboardButton("Карта"),
 		tgbotapi.NewKeyboardButton("👜 Инвентарь 👜"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("/menu"),
 	),
 )
 
@@ -45,6 +46,28 @@ var backpackKeyboard = tgbotapi.NewReplyKeyboard(
 	),
 	tgbotapi.NewKeyboardButtonRow(
 		tgbotapi.NewKeyboardButton("/menu"),
+	),
+)
+
+var moveKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("⬛"),
+		tgbotapi.NewKeyboardButton("🔼"),
+		tgbotapi.NewKeyboardButton("⬛"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("◀️"),
+		tgbotapi.NewKeyboardButton("️⏺"),
+		tgbotapi.NewKeyboardButton("▶️"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("⬛"),
+		tgbotapi.NewKeyboardButton("🔽"),
+		tgbotapi.NewKeyboardButton("/menu"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("\U0001F9BA Шмот \U0001F9BA"),
+		tgbotapi.NewKeyboardButton("🍕 Еда 🍕"),
 	),
 )
 
@@ -69,18 +92,23 @@ func GetMessage(telegramApiToken string) {
 
 		newMessage := update.Message.Text
 
-		if newMessage == "Карта" {
-			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "\U0001F7E9\U0001F7E7\U0001F7E9\U0001F7E9\U0001F7E9\U0001F7E9🌲🐺\n\U0001F7E9\U0001F7E7\U0001F7E9\U0001F7E9\U0001F7E9\U0001F7E9🌲\U0001F7E9\n\U0001F7E9\U0001F7E7\U0001F7E7\U0001F7E7\U0001F7E7\U0001F7E9\U0001F7E9\U0001F7E9\n\U0001F7E9\U0001F7E7🌳🌲\U0001F7E7\U0001F7E9\U0001F7E9\U0001F7E9\n\U0001F7E9\U0001F7E7🚪🌳🐱\U0001F7E9\U0001F7E7\U0001F7E7\n\U0001F7E9\U0001FAA8🌳🌲\U0001F7E7\U0001F7E9\U0001F7E7\U0001FAA8\n\U0001FAA8\U0001F7E9\U0001F7E7\U0001F7E7\U0001F7E7\U0001F7E7\U0001F7E7\U0001FAA8\n\U0001F7E9\U0001F7E9\U0001F7E7🌳🍎\U0001F7E9\U0001F7E9\U0001F7E9")
-			fmt.Println()
-		} else if newMessage == "/start" {
+		switch newMessage {
+		case "Карта":
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "🏔⛰🗻⬜️\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9\U0001F7E9\U0001F7E9\U0001F7E9🚪\n⛰🗻⬜️⬜️\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9⛪️\U0001F7E8🏪\U0001F7E9\n☃️⬜️⬜️⬜️\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9\n⬜️⬜️⬜️🔥\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9\n\U0001F7E9\U0001F7E9\U0001FAB5\U0001F7E9\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9🏥\U0001F7E8🏦\U0001F7E9\n\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\n\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8🕦\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\n\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E8\n\U0001FAA8\U0001FAA8🐚\U0001F7E9\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9\U0001F7E9\U0001F7E9\U0001F7E9🍄\n\U0001F7E6\U0001F7E6\U0001F7E6\U0001F7E6\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9\U0001F7EB🍅\U0001F7EB🥔\n\U0001F7E6\U0001F7E6\U0001F7E6\U0001F7E6\U0001F7E9\U0001F7E8🐱\U0001F7E8\U0001F7E9🥕\U0001F7EB🌽\U0001F7EB\n\U0001F9CA\U0001F9CA\U0001F7E6\U0001F7E6\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9\U0001F7EB🍎\U0001F7EB🍓\n\U0001F9CA\U0001F9CA\U0001F7E6\U0001F7E6\U0001F7E9\U0001F7E8\U0001F7E8\U0001F7E8\U0001F7E9🌳🌿🌱🌵")
+		case "/start":
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Приветствую тебя, мистер "+update.Message.From.FirstName+" "+update.Message.From.LastName)
-		} else if newMessage == "/menu" {
+		case "/menu":
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Меню")
+			//fmt.Println("1) ", tgbotapi.NewReplyKeyboard(tgbotapi.NewKeyboardButtonRow(names2buttons(mainKeyboardNames)))
+			fmt.Println("2) ", mainKeyboard)
 			msg.ReplyMarkup = mainKeyboard
-		} else if newMessage == "👜 Инвентарь 👜" {
+		case "👜 Инвентарь 👜":
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Инвентарь")
 			msg.ReplyMarkup = backpackKeyboard
-		} else {
+		case "/move":
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Движение")
+			msg.ReplyMarkup = moveKeyboard
+		default:
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Сам ты "+update.Message.Text)
 		}
 
